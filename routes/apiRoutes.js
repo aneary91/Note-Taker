cbe41ebe0d08c0726d622a96ea0e72b82bcd2265
AllisonWonderland <aneary@gmail.com>
2021-06-07 14:28:35 -0500

    created get route for notes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const storer = require("../db/storer");
+
+router.get("/notes", (req, res) => {
+  storer
+    .readNotes()
+    .then((notes) => {
+      return res.json(notes);
+    })
+    .catch((err) => res.json(err));
+});
+
+module.exports = router;
